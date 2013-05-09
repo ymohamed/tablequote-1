73aabbfcfa6673a9f315b264394932bdd6d40485
Tyler Horvath <thorvath@freightquote.com>
2013-05-09 09:41:48 -0500

    heyyyy

--- Tablequote/Controllers/HomeController.cs
+++ Tablequote/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Tablequote.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to tablequote";
 
             return View();
         }
